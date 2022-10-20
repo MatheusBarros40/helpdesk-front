1549ca2cca13ffabc79ec1eab77c28f5ad136942
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-20 16:54:09 -0300

    componente home criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,11 +23,13 @@ import {MatListModule} from "@angular/material/list";
 
 //componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
