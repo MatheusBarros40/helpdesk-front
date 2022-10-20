677a6311901b61d7859e9b9170220cd31353a5fc
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-20 16:59:21 -0300

    componente home criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
-  {path:'', component:NavComponent},
+  {path:'', component:NavComponent, children:[
+      {path:'home', component:HomeComponent}
+    ]
+  },
 ];
 
 @NgModule({
