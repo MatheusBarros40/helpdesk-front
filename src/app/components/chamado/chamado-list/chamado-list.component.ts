2661cb4a718b62d1224905386d093acd48e15700
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-21 16:49:38 -0300

    FILTRO E METODO FINDALL

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {Chamado} from "../../../models/chamado";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatPaginator} from "@angular/material/paginator";
+import {ChamadoService} from "../../../services/chamado.service";
 
 @Component({
   selector: 'app-chamado-list',
@@ -19,18 +20,44 @@ export class ChamadoListComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
 
-  constructor() { }
+  constructor(private service: ChamadoService) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
+  findAll():void{
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  retornaStatus(status: any):string {
+    if (status == '0'){
+      return 'ABERTO'
+    } else if (status == '1'){
+      return 'EM ANDAMENTO'
+    }else{
+      return 'ENCERRADO'
+    }
+  }
 
+  retornaPrioridade(prioridade: any):string {
+    if (prioridade == '0'){
+      return 'BAIXA'
+    } else if (prioridade == '1'){
+      return 'MEDIA'
+    }else{
+      return 'ALTA'
+    }
+  }
 
   orderByStatus(status: any): void{
     let list: Chamado[] = []
