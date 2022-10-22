232f6ed6235762527710c993f3c42b3ef981da8d
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-22 16:20:17 -0300

    atualização

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    // baseUrl: 'https://helpdesk40.herokuapp.com'
-    baseUrl: 'http://localhost:8080'
+    baseUrl: 'https://helpdesk40.herokuapp.com'
+    // baseUrl: 'http://localhost:8080'
 }
 
